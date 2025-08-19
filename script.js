7c5a61e65e242e86b97d0cd23b93010fedd1a22e
Josh Gearheart <other.josh.gearheart@gmail.com>
2025-08-19 14:20:34 -0700

    adjust scroll height to show text

--- script.js
+++ script.js
@@ -33,9 +33,10 @@ function showTestimonials(n) {
 
 // show header text on scroll
 const hideText = $("#logo-text")
+const height = $("#bigLogo").outerHeight();
 $(window).scroll(function () {
   const windowPos = $(window).scrollTop();
-  if (windowPos >= (150)) {
+  if (windowPos >= (height)) {
     hideText.removeClass("hidden");
   }
   else {
